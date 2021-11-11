410a53db125c08e4b87b800827d09b88d52b9438
miguel soto <miguelsr.1084@gmail.com>
2021-11-11 12:53:32 -0500

    creamos la interface, para la persistencia de datos y la de ejecutar

--- interfaces/infrastructure.go
+++ interfaces/infrastructure.go
@@ -1,2 +1,13 @@
 package interfaces
 
+import "github.com/MikelSot/melody-backend/domain"
+
+type Runner interface {
+	Run()
+}
+
+type DataPersistence interface {
+	AddUser(user *domain.User)
+	DeleteUser(user *domain.User)
+	AddMessageQueue(message domain.Message)
+}
