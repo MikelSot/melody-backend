68044a243e09de68546331408b575b39d08b0909
miguel soto <miguelsr.1084@gmail.com>
2021-11-11 16:01:47 -0500

    router terminado

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,14 +1,27 @@
 package infrastructure
 
-//import (
-//	"github.com/MikelSot/melody-backend/data"
-//	"github.com/MikelSot/melody-backend/domain"
-//)
+import (
+	"github.com/MikelSot/melody-backend/domain"
+	"net/http"
+)
 
 type router struct {
-
+	serveMux   *http.ServeMux
+	hanbler    *handler
+	controller *domain.Controller
 }
 
-func comer () {
+func NewRouter(
+	s *http.ServeMux,
+	h *handler,
+	c *domain.Controller ) *router {
+	return &router{s, h, c}
+}
 
+func (ro router) Router() {
+	ro.serveMux.HandleFunc("/ws", ro.Request)
+}
+
+func (ro router) Request(w http.ResponseWriter, r *http.Request) {
+	ro.hanbler.handler(ro.controller, w, r)
 }
